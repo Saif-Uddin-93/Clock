1ca2d0e177a6b08fcc1934b5574fc4240efb46b4
Saif Uddin <admin@saifuddin.uk>
2024-04-24 17:16:58 +0100

    added hours

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let digiClock = $(".digital-clock");
 let time = dayjs().format('HH:mm:ss');
+let anaClock = $('.ac-border');
 
 function updateTime(){
     setInterval(()=>{
@@ -9,3 +10,56 @@ function updateTime(){
 }
 
 updateTime();
+
+const _style = {
+    'position': 'absolute',
+    'border': '1px black solid',
+    'display': 'inline-block ',
+    'textAlign': 'center',
+    'top':'40%',
+    'left': '45%',
+    'color': 'black',
+}
+
+const numArray = [
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  1,
+  2,
+]
+
+const hourStyle = (index) => {
+    const x = btnPosX(index), y = btnPosY(index);
+    console.log(`${y} ${x}`)
+    return `
+        "position: absolute;
+        inset: ${y}rem ${x}rem;
+        text-align: center;
+        width: fit-content;
+        height: fit-content;
+        font-size: 24px;"`
+}
+
+function calcAngle(arr, index){ // calculate radians not degrees
+    return index*Math.PI*2/arr.length
+}
+
+const btnPosX = (index, radius=10)=>{
+    return radius * Math.cos(calcAngle(numArray, index))
+}
+
+const btnPosY = (index, radius=10)=>{
+    return radius * Math.sin(calcAngle(numArray, index))
+}
+//console.log(hourStyle());
+numArray.map((hour, index)=>(
+    $('.hours').append($(`<div style=${hourStyle(index)}>${hour}</div>`))
+))
